d29dc1869d796804563938d7e4158d1467871a12
OskarP <ospra20@student.sdu.dk>
2022-08-18 10:44:19 +0000

    ospra rebase

---
+++ ci/push-docker.sh
@@ -0,0 +1,4 @@
+echo "$docker_password" | docker login --username "$docker_username" --password-stdin
+docker push "$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}" 
+docker push "$docker_username/micronaut-app:latest" &
+wait
