ac6df82ac5eb17c327a18d89f18216c2b0226149
OskarP <ospra20@student.sdu.dk>
2022-08-18 11:29:27 +0000

    some new stuff :D

--- ci/build-docker-frontend.sh
+++ ci/build-docker-frontend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}micronaut-app:latest" -t "${DockerRepo}micronaut-app:1.0-$Tag" frontend/
+docker build -t "${DockerRepo}frontend-app:latest" -t "${DockerRepo}frontend-app:1.0-$Tag" frontend/
